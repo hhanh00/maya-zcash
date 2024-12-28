4b9f991b3cd7693f6cf34dc7526a624bf1b55ac2
Hanh <hanh425@gmail.com>
2024-12-28 14:48:43 +1000

    feat: add CI script

--- setup-regtest.sh
+++ setup-regtest.sh
@@ -1,22 +1,22 @@
 #!/bin/sh
 set -x
-./src/zcashd --datadir=regtest --daemon
+zcashd --datadir=regtest --daemon
 sleep 10
-./src/zcash-cli --datadir=regtest generate 150
-./src/zcash-cli --datadir=regtest z_getnewaccount
-./src/zcash-cli --datadir=regtest z_getaddressforaccount 0
-UA=`./src/zcash-cli --datadir=regtest listaddresses | jq -r '.[0].unified[0].addresses[0].address'`
-./src/zcash-cli --datadir=regtest z_shieldcoinbase '*' $UA
+zcash-cli --datadir=regtest generate 150
+zcash-cli --datadir=regtest z_getnewaccount
+zcash-cli --datadir=regtest z_getaddressforaccount 0
+UA=`zcash-cli --datadir=regtest listaddresses | jq -r '.[0].unified[0].addresses[0].address'`
+zcash-cli --datadir=regtest z_shieldcoinbase '*' $UA
 sleep 5
-./src/zcash-cli --datadir=regtest z_getoperationresult
-./src/zcash-cli --datadir=regtest generate 10
+zcash-cli --datadir=regtest z_getoperationresult
+zcash-cli --datadir=regtest generate 10
 sleep 1
-./src/zcash-cli --datadir=regtest z_sendmany $UA '[{"address": "tmGys6dBuEGjch5LFnhdo5gpSa7jiNRWse6", "amount": 5.40}]' 1 null 'AllowRevealedRecipients'
+zcash-cli --datadir=regtest z_sendmany $UA '[{"address": "tmGys6dBuEGjch5LFnhdo5gpSa7jiNRWse6", "amount": 5.40}]' 1 null 'AllowRevealedRecipients'
 sleep 5
-./src/zcash-cli --datadir=regtest z_getoperationresult
-./src/zcash-cli --datadir=regtest generate 10
+zcash-cli --datadir=regtest z_getoperationresult
+zcash-cli --datadir=regtest generate 10
 sleep 1
-./src/zcash-cli --datadir=regtest z_sendmany $UA '[{"address": "tmP9jLgTnhDdKdWJCm4BT2t6acGnxqP14yU", "amount": 1.20}]' 1 null 'AllowRevealedRecipients'
+zcash-cli --datadir=regtest z_sendmany $UA '[{"address": "tmP9jLgTnhDdKdWJCm4BT2t6acGnxqP14yU", "amount": 1.20}]' 1 null 'AllowRevealedRecipients'
 sleep 5
-./src/zcash-cli --datadir=regtest z_getoperationresult
-./src/zcash-cli --datadir=regtest generate 10
+zcash-cli --datadir=regtest z_getoperationresult
+zcash-cli --datadir=regtest generate 10
